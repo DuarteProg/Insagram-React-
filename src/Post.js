4924a5cfad1c4653b066365e5b76ff5a9aeff702
Lucas Duarte <lucas.duartebarbosa@outlook.com>
2022-05-08 14:01:42 -0300

    projeto sem o bonus finalizado

--- src/Post.js
+++ src/Post.js
@@ -1,77 +1,57 @@
+function MenuPost(props){
+ return(
+  <div class="post">
+  <div class="topo">
+    <div class="usuario">
+      <img src={props.img} />
+      {props.nome}
+    </div>
+    <div class="acoes">
+      <ion-icon name="ellipsis-horizontal"></ion-icon>
+    </div>
+  </div>
+
+  <div class="conteudo">
+    <img src={props.img2} />
+  </div>
+
+  <div class="fundo">
+    <div class="acoes">
+      <div>
+        <ion-icon name="heart-outline"></ion-icon>
+        <ion-icon name="chatbubble-outline"></ion-icon>
+        <ion-icon name="paper-plane-outline"></ion-icon>
+      </div>
+      <div>
+        <ion-icon name="bookmark-outline"></ion-icon>
+      </div>
+    </div>
+
+    <div class="curtidas">
+      <img src="assets/img/respondeai.svg" />
+      <div class="texto">
+        Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+      </div>
+    </div>
+  </div>
+</div>
+ )
+}
+
+const posts = [
+  {img:"assets/img/meowed.svg", nome:"meowed", img2:"assets/img/gato-telefone.svg"},
+  {img:"assets/img/barked.svg", nome:"barked", img2:"assets/img/dog.svg"}
+]
+
 export default function Post(){
     return (
         <div class="posts">
-            <div class="post">
-              <div class="topo">
-                <div class="usuario">
-                  <img src="assets/img/meowed.svg" />
-                  meowed
-                </div>
-                <div class="acoes">
-                  <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-              </div>
+           {
+           posts.map(posts =>
+            <MenuPost img={posts.img} nome={posts.nome}  img2={posts.img2}/>
+           )
 
-              <div class="conteudo">
-                <img src="assets/img/gato-telefone.svg" />
-              </div>
-
-              <div class="fundo">
-                <div class="acoes">
-                  <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                  </div>
-                  <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                  </div>
-                </div>
-
-                <div class="curtidas">
-                  <img src="assets/img/respondeai.svg" />
-                  <div class="texto">
-                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="post">
-              <div class="topo">
-                <div class="usuario">
-                  <img src="assets/img/barked.svg" />
-                  barked
-                </div>
-                <div class="acoes">
-                  <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-              </div>
-
-              <div class="conteudo">
-                <img src="assets/img/dog.svg" />
-              </div>
-
-              <div class="fundo">
-                <div class="acoes">
-                  <div>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                  </div>
-                  <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                  </div>
-                </div>
-
-                <div class="curtidas">
-                  <img src="assets/img/adorable_animals.svg" />
-                  <div class="texto">
-                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                  </div>
-                </div>
-              </div>
-            </div>
+           }
           </div>
     )
 }
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,81 +1,53 @@
+function MenuSideBarUsuario(props){
+  return (
+  <div class="usuario">
+  <img src="assets/img/catanacomics.svg" />
+  <div class="texto">
+    <strong>{props.texto}</strong>
+    {props.nome}
+  </div>
+</div>
+  )
+}
+
+function MenuSideBarSugestao(props){
+  return (
+<div class="sugestao">
+              <div class="usuario">
+                <img src={props.img} />
+                <div class="texto">
+                  <div class="nome">{props.nome}</div>
+                  <div class="razao">{props.razao}</div>
+                </div>
+              </div>
+
+              <div class="seguir">Seguir</div>
+            </div>
+  )
+  }
+
+  const sugestoes = [
+    {img:"assets/img/bad.vibes.memes.svg", nome:"bad.vibes.memes", razao:"Segue você"},
+    {img:"assets/img/chibirdart.svg", nome:"chibirdart", razao:"Segue você"},
+    {img:"assets/img/razoesparaacreditar.svg", nome:"razoesparaacreditar", razao:"Novo no Instagram"},
+    {img:"assets/img/adorable_animals.svg", nome:"adorable_animals", razao:"Segue você"},
+    {img:"assets/img/smallcutecats.svg", nome:"smallcutecats", razao:"Segue você"},
+  ]
+
 export default function SideBar(){
     return (
         <div class="sidebar">
-          <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
-            <div class="texto">
-              <strong>catanacomics</strong>
-              Catana
-            </div>
-          </div>
-
+          <MenuSideBarUsuario texto="catanacomics" nome="Catana" />
           <div class="sugestoes">
             <div class="titulo">
               Sugestões para você
               <div>Ver tudo</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/bad.vibes.memes.svg" />
-                <div class="texto">
-                  <div class="nome">bad.vibes.memes</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/chibirdart.svg" />
-                <div class="texto">
-                  <div class="nome">chibirdart</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/razoesparaacreditar.svg" />
-                <div class="texto">
-                  <div class="nome">razoesparaacreditar</div>
-                  <div class="razao">Novo no Instagram</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  <div class="nome">adorable_animals</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-
-            <div class="sugestao">
-              <div class="usuario">
-                <img src="assets/img/smallcutecats.svg" />
-                <div class="texto">
-                  <div class="nome">smallcutecats</div>
-                  <div class="razao">Segue você</div>
-                </div>
-              </div>
-
-              <div class="seguir">Seguir</div>
-            </div>
-          </div>
-
+            </div>     
+{
+            sugestoes.map(sugestoes =>  
+          <MenuSideBarSugestao img={sugestoes.img} nome={sugestoes.nome} razao={sugestoes.razao} />
+             ) 
+}
           <div class="links">
             Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
           </div>
@@ -83,6 +55,7 @@ export default function SideBar(){
           <div class="copyright">
             © 2021 INSTAGRAM DO FACEBOOK
           </div>
+        </div>
         </div>
     )
 }
